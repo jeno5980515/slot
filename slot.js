6574956e6ee164b3f9dcbb80e654b3bcb07dcf9c
jeno5980515 <jeno5980515@yahoo.com.tw>
2017-09-12 00:31:30 +0800

    blur

--- slot.js
+++ slot.js
@@ -6,10 +6,15 @@ let speed = 0.5;
 const decelerate = 0.025; 
 const speedBound = 5;
 const beginDecreaseBound = 50;
+let blur = 12;
 let timer = 0;
+
+const wrapper = document.querySelector('.slot .wrapper');
+wrapper.style.color = 'transparent';
 
 const animate = () => {
   if ( timer >= beginDecreaseBound && timer % speedBound === 0 ){
+    blur --;
     speed = speed > decelerate ? speed - decelerate : decelerate
   }
  
@@ -18,13 +23,13 @@ const animate = () => {
   if ( currentIndex >= itemLength ){
     currentIndex = 0;
   }
-  
-  document.querySelector('.slot .wrapper').style = 
-    `transform: matrix(1, 0, 0, 1, 0, -${currentIndex * borderHeight})`;
+
+  wrapper.style.textShadow = `0 0 ${blur}px rgba(0,0,0,1)`;
+  wrapper.style.transform = `matrix(1, 0, 0, 1, 0, -${currentIndex * borderHeight})`;
 
   if ( timer <= TIME || (timer > TIME && !Number.isInteger(currentIndex)) ) {
     requestAnimationFrame(animate);
-  }
+  } 
 }
 
 requestAnimationFrame(animate);
